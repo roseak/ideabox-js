0ee74b98fb6e6febd76995bae5aa81c2fce30428
roseak <rose.kohn@gmail.com>
2015-11-30 17:07:29 -0700

    Added the ability to append ideas to the page.

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'jquery-ui-rails'
 gem 'rails_12factor', group: :production
-gem 'responders', '~> 2.0'
+gem 'responders'
 
 group :development, :test do
   gem 'pry'
--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-  $.getJSON('/api/v1/ideas')
+  $.getJSON('/api/v1/ideas', function(data) {
+    $.each(data, function(index, idea){
+      renderIdeas(idea)
+    })
+  })
     .then(function(data){ console.log('It Worked')})
     .fail(function(data){ console.log('It Failed :(')})
     .always(function(data){ console.log('Stuff is happening.')})
+
 });
+
+function renderIdeas(idea) {
+  $('#idea-listing').append(
+    "<h3>"
+    + idea.title
+    + "</h3><p>"
+    + idea.body
+    + "</p>"
+  )
+};
