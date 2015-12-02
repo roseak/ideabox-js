613ba3e284b88a4d82a7a6dc66a6580f08c67d98
roseak <rose.kohn@gmail.com>
2015-12-02 16:21:10 -0700

    Added validations for the idea model.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,6 @@
 class Idea < ActiveRecord::Base
+  default_scope { order('created_at') }
+  validates :title, presence: true
+  validates :body, presence: true, uniqueness: true
   enum quality: %w(swill plausible genius)
 end
