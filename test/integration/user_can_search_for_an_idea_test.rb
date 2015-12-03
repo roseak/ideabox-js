afcab1a46f5e2ebfd873d13008c0ca9e48d4bfb1
roseak <rose.kohn@gmail.com>
2015-12-02 21:50:40 -0700

    Added test for searching by a title.

---
+++ test/integration/user_can_search_for_an_idea_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class UserCanSearchForAnIdeaTest < ActionDispatch::IntegrationTest
+  def teardown
+    Capybara.reset_sessions!
+  end
+
+  test "user can search by title" do
+    visit "/"
+    find("#filter").click
+    fill_in("filter", with: "testing")
+    assert page.has_content?("For All The Green Dots")
+    refute page.has_content?("Pizza and Cookies")
+  end
+end
