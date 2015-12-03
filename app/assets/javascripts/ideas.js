0b96149f486638d2b145f43be92c30ac11baae68
roseak <rose.kohn@gmail.com>
2015-12-02 22:25:06 -0700

    Added hover color to indicate the idea title and body are editable.

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -17,8 +17,8 @@ function renderIdeas(idea) {
     "<li class='collection-item idea' data-id='" + idea.id
     + "' data-quality='" + idea.quality
     + "'><div class='row' id='idea-item'><div class='col s1' id='thumbs'>"
-    + "<i class='material-icons tooltip' id='thumbs-up-idea'>thumb_up</i>"
-    + "<i class='material-icons tooltip' id='thumbs-down-idea'>thumb_down</i></div>"
+    + "<i class='material-icons' id='thumbs-up-idea'>thumb_up</i>"
+    + "<i class='material-icons' id='thumbs-down-idea'>thumb_down</i></div>"
     + "<div class='col s10' id='idea-meat'>"
     + "<h5 contenteditable='true' class='title-editable'>"
     + idea.title
