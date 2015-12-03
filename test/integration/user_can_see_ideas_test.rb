7591264f5f3f505eeebe06210fcaeac2cb13a9ac
roseak <rose.kohn@gmail.com>
2015-12-02 19:41:24 -0700

    Added tests to see that a user can see ideas, that they're sorted by when they were created, and are truncated to the nearest word.

---
+++ test/integration/user_can_see_ideas_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class UserCanSeeIdeasTest < ActionDispatch::IntegrationTest
+  def teardown
+    Capybara.reset_sessions!
+  end
+
+  test "user can see ideas on the page" do
+    visit "/"
+    assert page.has_content?("Testing")
+    assert page.has_content?("For All The Green Dots")
+    assert page.has_content?("swill")
+  end
+
+  test "ideas appear based on when they were created" do
+    visit "/"
+    assert page.all(".idea").first.has_content?("Dinner")
+  end
+
+  test "ideas are truncated by the word when longer than 100 characters" do
+    visit "/"
+    save_and_open_page
+    assert page.has_content?("When truncated, the idea is harder to see.  But who really cares about this dumb idea anyway? ...")
+  end
+end
